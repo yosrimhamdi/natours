5275d7eb049ff97dfde52d79b87241af3eb98d25
yossri mhamdi <bavary1515@gmail.com>
2020-08-09 11:20:16 +0100

    getting only the active users.

--- models/user.js
+++ models/user.js
@@ -46,6 +46,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordRestExpiresIn: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -61,6 +66,12 @@ userSchema.pre('save', function (next) {
   if (this.isModified('password') && !this.isNew) {
     this.passwordChangedAt = Date.now() - 1000;
   }
+
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
 
   next();
 });
