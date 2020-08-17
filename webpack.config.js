f4d2bc5d8080d8bd3026976b4b1b4c04fbffabb7
yossri mhamdi <bavary1515@gmail.com>
2020-08-17 13:17:01 +0100

    added babel webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,18 @@ module.exports = {
   },
   mode: 'development',
   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
